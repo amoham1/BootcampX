e897b0d419d19f285a36b0aa6ee2ca03623f3bea
amoham1 <amoham456@gmail.com>
2021-11-22 20:35:16 -0500

    group by question

--- migrations/assignments_submissions.sql
+++ migrations/assignments_submissions.sql
@@ -15,3 +15,23 @@ CREATE TABLE assignment_submissions (
   submission_date DATE
 );
 
+
+SELECT students.name as student, count(assignment_submissions.*) as total_submissions
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+WHERE students.end_date IS NULL
+GROUP BY students.name
+HAVING count(assignment_submissions.*) < 100;;
+
+SELECT day, count(assignments.*) as total_assigments 
+FROM assignments
+GROUP BY day
+HAVING count(assignments.*) >= 10
+ORDER BY day;
+
+SSELECT cohorts.name as cohort_name, count(students.*) AS student_count 
+FROM cohorts
+JOIN students ON cohorts.id = cohort_id
+GROUP BY cohort_name 
+HAVING count(students.*) >= 18
+ORDER BY student_count;

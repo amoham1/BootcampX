137887677bc72d69155132d89d41bed01f4b8642
amoham1 <amoham456@gmail.com>
2021-11-25 14:44:46 -0500

    sanizting the querys

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,17 +7,22 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests) as total_assistances
+const queryString = `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-GROUP BY teachers.name, cohorts.name
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`).then(res => {
+`;
+
+const cohortName = [ process.argv[2] || 'JUL02' ];
+
+pool.query(queryString, cohortName)
+.then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
   })
-});
+})
+.catch(err => console.error('query error', err.stack));
